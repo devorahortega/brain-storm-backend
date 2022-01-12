1c2b533468ffa69b0f9016509fe628973d74a0b2
Devorah Ortega <devorahb.ortega@gmail.com>
2022-01-12 10:13:57 -0500

    Creates database, migrates and adds create action to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+  def create
+    user = User.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone: params[:phone],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation],
+    )
+
+    if user.save
+      render json: user
+    else
+      render json: { errors: user.errors.full_messages }, status: 400
+    end
+  end
 end
