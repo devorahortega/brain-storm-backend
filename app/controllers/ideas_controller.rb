a2375688d5cef7acb440a9b4892ea9840010e5b1
Devorah Ortega <devorahb.ortega@gmail.com>
2022-02-14 22:35:42 -0600

    adds ideas controller

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,2 +1,43 @@
 class IdeasController < ApplicationController
+  def index
+    prompts = Idea.all
+    render json: ideas
+  end
+
+  def show
+    idea = Idea.find_by(id: params[:id])
+    render json: idea
+  end
+
+  def create
+    idea = idea.new(
+      idea: params["idea"],
+    )
+    if idea.save
+      render json: idea
+    else
+      render json: { errors: idea.error.full_messages }, status: 422
+    end
+  end
+
+  def update
+    idea_id = params["id"]
+    idea = Idea.find_by(id: idea_id)
+
+    idea.idea = params["idea"] || idea.idea
+
+    if idea.save
+      render json: idea
+    else
+      render json: { errors: idea.error.full_messages }, status: 422
+    end
+  end
+
+  def destroy
+    idea_id = params["id"]
+    idea = idea.find_by(id: idea_id)
+
+    idea.destroy
+    render json: { message: "This idea has been destroyed" }
+  end
 end
