8f0a4a3129fc6656473f70aead02a7eee47fd5c7
Devorah Ortega <devorahb.ortega@gmail.com>
2022-04-12 11:16:10 -0400

    Fixes issue with user create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,10 @@ class UsersController < ApplicationController
     user = User.new(
       first_name: params[:first_name],
       last_name: params[:last_name],
-      email: params[:email]
+      email: params[:email],
       phone: params[:phone],
       password_confirmation: params[:password_confirmation],
     )
-
     if user.save
       render json: user
     else
