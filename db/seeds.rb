330dfb6c871b073499eaa02b309291bdaaacd748
Devorah Ortega <devorahb.ortega@gmail.com>
2022-08-25 13:15:11 -0500

    Adds ideas in seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,5 @@
 # Examples:
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ideas = Idea.create([{ idea: "What do you know to be true that you didn’t know a year ago?" }, { idea: "Write a list of all the things you are worried about right now." }, { idea: "What tasks are you avoiding right now?" }, { idea: "If you could live/travel anywhere, where would it be and why?" }, { idea: "What emotions are you feeling right now?" }, { idea: "When you look in the mirror what do you see?" }, { idea: "What does a perfect day look like to you?" }, { idea: "What is your relationship with people like?" }, { idea: "I got where I am today because ____." }, { idea: "What things do you do to destress, and do they work?" }])
