441299d32b9fe2a21e5abe485175a0c4b194a237
Devorah Ortega <devorahb.ortega@gmail.com>
2022-09-01 12:44:14 -0500

    changes authorization for ideas

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,5 +1,5 @@
 class IdeasController < ApplicationController
-  before_action :authenticate_user, except: [:index, :show]
+  #before_action :authenticate_user, except: [:index, :show]
 
   def index
     ideas = Idea.all
